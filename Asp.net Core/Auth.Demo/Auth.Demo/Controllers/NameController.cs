f0b894f6528ee7afeec79aacd91eb34f5ed2c310
sohag16030 <shohag.mbstu.it16030@gmail.com>
2021-06-26 17:50:56 +0600

    updated

---
+++ Asp.net Core/Auth.Demo/Auth.Demo/Controllers/NameController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Demo.Controllers
+{
+    public class NameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserCred userCred)
+        {
+            return Ok();
+        }
+    }
+}
