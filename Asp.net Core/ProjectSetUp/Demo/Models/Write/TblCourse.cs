4fb3e6ac293bc5a47b20cad8c2101e5042d638bd
sohagsadaf <shohag.mbstu.it16030@gmail.com>
2021-07-02 15:23:10 +0600

    demo configuration complete

---
+++ Asp.net Core/ProjectSetUp/Demo/Models/Write/TblCourse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestWeb.Models.Write
+{
+    public partial class TblCourse
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public string Course { get; set; }
+        public int Credit { get; set; }
+        public decimal CreditFee { get; set; }
+    }
+}
---
+++ Asp.net Core/ProjectSetUp/Demo/Models/Write/TblStudent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestWeb.Models.Write
+{
+    public partial class TblStudent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public decimal? Cgpa { get; set; }
+    }
+}
