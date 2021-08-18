8a6ae8109a79f4fc9bc31eb5f128a60804c46c51
sohagsadaf <shohag.mbstu.it16030@gmail.com>
2021-08-18 12:32:31 +0600

    event

---
+++ Asp.net Core/CSharpAdvTopics/Event/EmployeeSeperator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Event
+{
+    public class EmployeeSeperator
+    {
+        public delegate void EmployeeSeparatedEventHandler();
+        public event EmployeeSeparatedEventHandler EmployeeSeparatedEvent;
+
+        public void Seperator()
+        {
+            if(EmployeeSeparatedEvent!=null)
+              EmployeeSeparatedEvent();
+        }
+    }
+}
---
+++ Asp.net Core/CSharpAdvTopics/Event/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Event
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeSeperator obj = new EmployeeSeperator();
+            Finance obj2 = new Finance(obj);
+
+            obj.Seperator();
+
+        }
+    }
+}
