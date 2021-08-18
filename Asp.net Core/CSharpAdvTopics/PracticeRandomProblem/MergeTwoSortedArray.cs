574fa0bcde25bf6dbd1a9b0783cdcb166e2d57bf
sohagsadaf <shohag.mbstu.it16030@gmail.com>
2021-08-18 16:20:10 +0600

    updated

--- Asp.net Core/CSharpAdvTopics/PracticeRandomProblem/MergeTwoSortedArray.cs
+++ Asp.net Core/CSharpAdvTopics/PracticeRandomProblem/MergeTwoSortedArray.cs
@@ -10,13 +10,13 @@ namespace PracticeRandomProblem
         {
             int[] arr1 = { 1, 5, 7, 12, 13 };
             int[] arr2 = { 2, 3, 6, 7, 8 };
-            // output: 1,2,35,6,7,8,12,13
-            int[] arr3 = new int[10];
+            // output: 1,2,3,5,6,7,8,12,13
             var n = arr1.Length; var m = arr2.Length;
+            int[] arr3 = new int[n+m];
+
             int a = 0, b = 0, c = 0;
-            for (int i = 0; i < n+m; i++)
+            while(a<n && b<m)
             {
-                if( a == n-1 || b == m-1)  break;
 
                 if (arr1[a] <= arr2[b])
                 {
@@ -32,21 +32,19 @@ namespace PracticeRandomProblem
             }
             if (a < n)
             {
-                for (int j = a; j < n; j++)
+                while (a < n)
                 {
-                    arr3[c] = arr1[j];
-                    j++; c++;
-
+                    arr3[c] = arr1[a];
+                    a++;c++;
                 }
 
             }
             else if (b < m)
             {
-                for (int k = a; k < m; k++)
+                while (b < m)
                 {
-                    arr3[c] = arr2[k];
-                    k++; c++;
-
+                    arr3[c] = arr2[b];
+                    b++; c++;
                 }
 
             }
