ad948082922ad177dc977002808a3ac26f7b4508
sohagsadaf <shohag.mbstu.it16030@gmail.com>
2021-08-19 18:00:22 +0600

    GCD

---
+++ Asp.net Core/CSharpAdvTopics/PracticeRandomProblem/GCD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeRandomProblem
+{
+    class GCD
+    {
+        public static void Main()
+        {
+            int a = 18, b = 2;
+            Console.WriteLine(Gcd(a, b));
+        }
+        private static int Gcd(int a, int b)
+        {
+            if (a == 0) return b;
+            else
+                return Gcd(b % a, a);
+        }
+    }
+}
