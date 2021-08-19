e8450b43cabd57b5e23cfc6d45a15b107ad1de12
sohagsadaf <shohag.mbstu.it16030@gmail.com>
2021-08-19 15:50:33 +0600

    recursion

---
+++ Asp.net Core/CSharpAdvTopics/RecursionPratice/Fibonacci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecursionPratice
+{
+    class Fibonacci
+    {
+        public static void Main()
+        {
+            var n = 3;
+            Console.WriteLine( Fib(n));
+        }
+
+        public  static int Fib(int n)
+        {
+            if (n == 1 || n == 0) return 1;
+            else 
+                return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
---
+++ Asp.net Core/CSharpAdvTopics/RecursionPratice/Program.cs
@@ -0,0 +1,12 @@
+﻿//using System;
+
+//namespace RecursionPratice
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//        }
+//    }
+//}
