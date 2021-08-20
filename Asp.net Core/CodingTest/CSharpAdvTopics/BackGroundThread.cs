42332fbec81e5d584400b9bc17e0ee1000ec1086
sohag16030 <shohag.mbstu.it16030@gmail.com>
2021-08-20 18:56:46 +0600

    c#adv

---
+++ Asp.net Core/CodingTest/CSharpAdvTopics/BackGroundThread.cs
@@ -0,0 +1,28 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Threading;
+
+//namespace CSharpAdvTopics
+//{
+//    class BackGroundThread
+//    {
+//        public static void Main()
+//        {
+//            var thr = new Thread(mythread);
+//            thr.Start();
+//            thr.IsBackground = true;
+//            Console.WriteLine("Main Thread is completed");
+//        }
+
+//        private static void mythread(object obj)
+//        {
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine("MyThread is running");
+//                Thread.Sleep(1000);
+//            }
+//            Console.WriteLine("My Thread is completed");
+//        }
+//    }
+//}
---
+++ Asp.net Core/CodingTest/CSharpAdvTopics/ForeGroundThreadExample.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Threading;
+
+//namespace CSharpAdvTopics
+//{
+//    class ForeGroundThreadExample
+//    {
+//        public static void Main()
+//        {
+//            Thread thr = new Thread(MyThread);
+//            thr.Start();
+//            Console.WriteLine("Main Thread is complete");
+
+//        }
+//        private static void MyThread()
+//        {
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine("MyThread is running");
+//                Thread.Sleep(1000);
+//            }
+//            Console.WriteLine("MyThread is completed");
+//        }
+//    }
+//}
