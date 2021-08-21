03805fc3d9ad320256a80dbabec425cf45716455
sohagsadaf <shohag.mbstu.it16030@gmail.com>
2021-08-21 16:33:56 +0600

    updated

---
+++ Asp.net Core/CodingTest/OOP_OOD/StructVsClass.cs
@@ -0,0 +1,50 @@
+﻿//using System;
+
+//namespace OOP_OOD
+//{
+//    class StructVsClass
+//    {
+//        //struct Location // Struct is value type
+//        //{ 
+//        //    public string Name { get; set; }
+//        //    public int Distance { get; set; }
+
+//        //    public Location(string name,int distance)
+//        //    {
+//        //        Name = name;
+//        //        Distance = distance;
+//        //    }
+
+//        //    public string GetInfo()
+//        //    {
+//        //        var ans = $"{Name} {Distance}";
+//        //        return ans;
+//        //    }
+//        //}
+//        public class Location // class is reference type
+//        {
+//            public string Name { get; set; }
+//            public int Distance { get; set; }
+
+//            public Location(string name, int distance)
+//            {
+//                Name = name;
+//                Distance = distance;
+//            }
+
+//            public string GetInfo()
+//            {
+//                var ans = $"{Name} {Distance}";
+//                return ans;
+//            }
+//        }
+//        static void Main(string[] args)
+//        {
+//            var obj = new Location("Dhaka", 5000);
+//            var obj2 = obj;
+//            obj2.Distance = 2000;
+//            Console.WriteLine(obj.GetInfo());
+//            Console.WriteLine(obj2.GetInfo());
+//        }
+//    }
+//}
--- Asp.net Core/CodingTest/PracticeRandomProblem/BubbleSort.cs
+++ Asp.net Core/CodingTest/PracticeRandomProblem/BubbleSort.cs
@@ -4,11 +4,24 @@
 
 //namespace PracticeRandomProblem
 //{
-//    class BubbleSort
+//    class BubbleSort //complexity O(n)
 //    {
 //        public static void Main()
 //        {
-
+//            var arr = new List<int> { 6, 4, 2, 7, 9, 1 };
+//            for (int i = 0; i < arr.Count; i++)
+//            {
+//                var temp=0;
+//                for (int j = i; j < arr.Count; j++)
+//                {
+//                    if (arr[i] > arr[j])
+//                    {
+//                        temp = arr[j];
+//                        arr[j] = arr[i];
+//                        arr[i] = temp;
+//                    }
+//                }
+//            }
 //        }
 //    }
 //}
