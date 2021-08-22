4e67c94954264468fbc2ef75d7a63e52cd31901a
sohag16030 <shohag.mbstu.it16030@gmail.com>
2021-08-22 23:52:34 +0600

    recursion

---
+++ Asp.net Core/CodingTest/RecursionPratice/SumOfDigits.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecursionPratice
+{
+    class SumOfDigits
+    {
+        public static int sum;
+        public static void Main(string[] args)
+        {
+            var n = 123;
+            SumOfDigitsfun(n);
+        }
+
+        private static int SumOfDigitsfun(int n)
+        {
+            if (n != 0)
+            {
+                sum += n % 10;
+                return SumOfDigitsfun(n / 10);
+            }
+            return sum;
+        }
+    }
+}
