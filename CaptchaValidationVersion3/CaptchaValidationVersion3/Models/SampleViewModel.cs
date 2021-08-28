92bcead78be86903170db90864650108b5b5a784
sohagsadaf <shohag.mbstu.it16030@gmail.com>
2021-08-28 16:07:53 +0600

    successfuly done

---
+++ CaptchaValidationVersion3/CaptchaValidationVersion3/Models/SampleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CaptchaValidationVersion3.Models
+{
+    public class SampleViewModel
+    {
+        public string Text { get; set; }
+        public string Captcha { get; set; }
+    }
+}
