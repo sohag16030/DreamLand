7129b66426932baf78637a6f629c4e245a2d94a9
sohag16030 <shohag.mbstu.it16030@gmail.com>
2021-08-31 19:59:34 +0600

    ActionActivityControlling Done

--- StoreManagementSystem/StoreManagementSystem/Controllers/ActionActivityController.cs
+++ StoreManagementSystem/StoreManagementSystem/Controllers/ActionActivityController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StoreManagementSystem.DbContexts;
+using StoreManagementSystem.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,53 @@ namespace StoreManagementSystem.Controllers
     public class ActionActivityController : Controller
     {
         private readonly MyDbContext _context;
-        
-        [HttpGet]
-        public IActionResult AddProductActivity(string val)
+        public ActionActivityController(MyDbContext context)
         {
-            
-            return View();
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<ActionActivity> list = _context.ActionActivities.ToList();
+            return View(list);
+        }
+        [HttpPut]
+        public IActionResult Active(int actionId)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var status = (from x in _context.ActionActivities where x.ActionId == actionId select x).FirstOrDefault();
+                    status.ActionStatus =true ;
+                    _context.Update(status);
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    return Redirect("Index");
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPut]
+        public IActionResult InActive(int actionId)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var status = (from x in _context.ActionActivities where x.ActionId == actionId select x).FirstOrDefault();
+                    status.ActionStatus = false;
+                    _context.Update(status);
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    return Redirect("Index");
+                }
+            }
+            return RedirectToAction("Index");
         }
     }
 }
